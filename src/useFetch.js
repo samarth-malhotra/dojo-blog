47bc7fbed0092c927b084ebef7efbf1d9aa3bec9
Samarth Malhotra <malhotra.samarth@hotmail.com>
2023-01-09 19:40:58 +0530

    as per latest observation

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,8 +4,7 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
-    const abortCont = new AbortController();
-    fetch(url, { signal: abortCont.signal })
+    fetch(url)
       .then((res) => {
         if (!res.ok) throw Error("Could not fetch the data for that resource");
         return res.json();
@@ -16,14 +15,9 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        if (err.name === "AbortError") {
-          console.log("Fetch aborted");
-        } else {
-          setIsPending(false);
-          setError(err.message);
-        }
+        setIsPending(false);
+        setError(err.message);
       });
-    return () => abortCont.abort();
   }, [url]);
   return { data, isPending, error };
 };
